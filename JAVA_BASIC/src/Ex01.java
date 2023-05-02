360436ff7e0ac14903679b633a34f5df233f1711
minaci1 <minaci_@naver.com>
2023-05-02 19:57:12 +0900

    feat:modify

--- JAVA_BASIC/src/Ex01.java
+++ JAVA_BASIC/src/Ex01.java
@@ -5,7 +5,7 @@ public class Ex01 {
 		System.out.println("hello"); // Ctrl + F11 -> 컴파일 단축키
 		// 저장시 컴파일 + 실행파일까지 생성
 		
-		// 자동차 생산
+		// 자동차 생산11
 		// Car mycar -> stack이라는 메모리에 만들어진다.
 		Car mycar = new Car(); // new를 이용해 자동차를 만든다. heap 메모리에 올라간다.
 		mycar.setDoor(2);
